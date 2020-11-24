5f08b26e2672dab48bdc5943c5f00552f696c01c
Marek Böhm <Marek.Bhm@seznam.cz>
2020-11-24 16:28:51 +0100

    Replaced delay() with _delay_ms() for better accuracy

--- 10Hz_to_1Hz/10Hz_to_1Hz.ino
+++ 10Hz_to_1Hz/10Hz_to_1Hz.ino
@@ -1,4 +1,5 @@
-
+// include library for proper use of 
+#include <util/delay.h>
 
 // 1 Hz signal pin
 int oneHz = 11;
@@ -6,17 +7,20 @@ int oneHz = 11;
 // interrupt pin 10 Hz
 int interruptPin = 2;
 
+// duration of interrupt
+volatile unsigned long Duration = 0;
+
 // the flag for the 1 Hz signal
 volatile boolean flag = false;
 
+// interrupt counter
 volatile int count = 0;
 
-// half period
-unsigned long halfPeriod = 50000000UL; // 500mS = 1/2 Hz
-
-
-
 void setup() {
+
+  // Set up serial
+  //Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  
   // put your setup code here, to run once:
 
   // define the pinModes
@@ -29,23 +33,30 @@ void setup() {
   digitalWrite (11, LOW); // known starting level
 }
 
-
-
 void loop() {
   // put your main code here, to run repeatedly:
   if (flag){ // avoid  loop() jitter
 
     // time critical - turn off interrupts
     //noInterrupts();
-  
-    PINB = PINB | 0b00001000; // x-x-13-12-11-10-9-8 on Uno,
-                                             // toggle output by writing 1 to input register
-    delay(500);
 
+    // toggle output by writing 1 to input register
+    PINB = PINB | 0b00001000; // x-x-13-12-11-10-9-8 on Uno,
+
+
+    _delay_ms(1000);
+    //delay(1000);
+
+    // replace the delay with a 
+
+    PINB = PINB | 0b00000000; // x-x-13-12-11-10-9-8 on Uno,
 
 
     // set flag back to false
     flag = false;
+
+    // print interrupt duration
+    // Serial.println(Duration, DEC);
     
     // not time critical
     //interrupts(); 
@@ -56,13 +67,19 @@ void loop() {
 // if in the ISR, change the flag to true
 void foo() {
 
+  unsigned long then = micros();
   
   count++;
 
   if (count % 10 == 0) {
-    
+
+
+   // set the flag to true
    flag = true;  
-  
+
+   // reset the counter
+   count = 0;
+   
   }
 
   else {
@@ -71,6 +88,8 @@ void foo() {
     
   }
 
+  //unsigned long now = micros();
+  //Duration = now - then;
   
 
   
